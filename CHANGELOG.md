dce9bc9ccfa9c2f3bbec862be5fddd3cb5193a5c
Daniel Díaz <diaz_carrete@yahoo.com>
2022-04-03 14:28:21 +0200

    First commit.

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Revision history for lazy-bracket
+
+## 0.1.0.0 -- YYYY-mm-dd
+
+* First version. Released on an unsuspecting world.
---
+++ shell.nix
@@ -0,0 +1,9 @@
+let
+  myNixPkgs = import <nixpkgs> {};
+in
+myNixPkgs.mkShell {
+  nativeBuildInputs = with myNixPkgs; [
+    cabal-install # terminal app cabal
+    ghc # Haskell compiler
+  ];
+}
